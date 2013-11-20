0eaeb3061fe1f77ca7b04baf7721af0b716485ea
kornobis <ekornobis@gmail.com>
2013-11-20 09:08:14 +0000

    fixing paths errors

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 
-REMOTEHOST = "kornobis@altamira1.ifca.es"
-REMOTEHOME = "/gpfs/csic_users/kornobis/webserver/"
+REMOTEHOST = "genorama@altamira1.ifca.es"
+REMOTEHOME = "pipe_out"
+PIPE_LAUNCH = "../server_side/pipe_launcher.py"
+
+# for stable
+#PIPE_LAUNCH = "../../server_side/stable/pipe_launcher.py"
