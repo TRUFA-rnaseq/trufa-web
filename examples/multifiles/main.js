ae4a69404477d42dfee624d117a31c3cc585b323
Luis Cabellos <cabellos@ifca.unican.es>
2013-11-20 08:42:52 +0000

    add multifile example

---
+++ examples/multifiles/main.js
@@ -0,0 +1,61 @@
+/* = JOB STUFF ============================================================== */
+!function ($) {
+    "use strict"; // jshint ;_;
+
+    $('#add_multifile').click( function(){
+	$('#multifilelist').append('<input class="multifileitem" type="text"></input>');
+    });
+
+    $('#jobstart').click( function(){
+
+	var multifiles = [];
+	$("input.multifileitem").each(function(){
+	    var str = $(this).val();
+	    if( str.length > 0 ){
+		multifiles.push( $(this).val() );
+	    }
+	});
+	
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function( e ){
+            if( 4 == this.readyState ){
+                alert( "job sended" );
+                refreshJobList();
+            }
+        };
+
+        xhr.open( 'POST', '/ajax/job', true );
+	
+        var form = $('#jobform')[0];
+        var fd = new FormData( form );
+	fd.append( 'multifiles', multifiles );
+        xhr.send( fd );
+    });
+
+    refreshJobList();
+
+}(window.jQuery);
+
+function refreshJobList(){
+    $.ajax({
+        dataType: "json",
+        url: '/ajax/job',
+        success: function( data ) {
+            var items = [];
+            var jobs = data['jobs']
+
+            $.each( jobs, function( key, val ) {
+                items.push('<li><a href="/job/' + val['id'] + '">Job ' + val['id'] + '</a></li>');
+            });
+
+            var newlist = $('<ul/>', {
+                'id': 'joblist',
+                'class': 'unstyled',
+                html: items.join('')
+            });
+
+            $('#joblist').replaceWith( newlist );
+        }
+    });
+}
