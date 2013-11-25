005b0b790a27dd0c78b72f37bd1ef4003ade1b00
kornobis <ekornobis@gmail.com>
2013-11-25 11:54:54 +0000

    Connecting "seq database" tagged files to custom blat dropdown-menus

--- static/js/run_job.js
+++ static/js/run_job.js
@@ -10,13 +10,15 @@
 
 
 function fillupBlatMenu1(){
-    getFileListWithType( 1, function( files ){
+    getFileListWithType( 3, function( files ){
         var items1 = [];
 
         $.each(files, function( key, val ) {
             items1.push( "<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"#\"> " + val['file'] + "</a></li>" )
         });
 
+        $( '#blat_menu0').append( items1 );
         $( '#blat_menu1').append( items1 );
+        $( '#blat_menu2').append( items1 );
     });
 }
