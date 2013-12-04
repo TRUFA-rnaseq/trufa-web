cbc17529e105db9d236139b71a50b023bf433828
Luis Cabellos <cabellos@ifca.unican.es>
2013-12-04 13:45:15 +0000

    add setup page

---
+++ static/js/setup.js
@@ -0,0 +1,25 @@
+!function ($) {
+    "use strict"; // jshint ;_;
+
+    $('#update-button').click( function(){
+        var formstr = $('#setupform').serialize();
+
+        $.ajax({
+            dataType: "json",
+            url: '/ajax/me',
+            type: 'PUT',
+            data: formstr,
+
+            success: function( data ) {
+                $('#setupform input').val('')
+                showClear()
+                if( data['ok'] ){
+                    showOK( "Password Changed" )
+                }else{
+                    showError( data['msg'] )
+                }
+            }
+        });
+    });
+
+}(window.jQuery);
