1d036dc06da77f336ee10fb9dc3acd2e180be003
kornobis <ekornobis@gmail.com>
2014-01-30 13:48:58 +0000

    setting up a testing area (not interfering with the actual jobs submitted through trufa) >>> has to be edited after each deploy

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 VERSION = "0.4.1"
 
 REMOTEHOST = "genorama@altamira1.ifca.es"
-REMOTEHOME = "users"
+
+# for testing
+REMOTEHOME = "testing"
+DATADIR = "/gpfs/res_projects/cvcv/webserver/testing/"
+
+# for stable
+#REMOTEHOME = "users"
+#DATADIR = "/gpfs/res_projects/cvcv/webserver/users/"
+
 PIPE_LAUNCH = "../server_side/pipe_launcher.py"
 
 # for stable
