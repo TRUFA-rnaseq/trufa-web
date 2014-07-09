08f1ca600d8db56dd98c090965bfd002c1db9e43
Luis Cabellos <cabellos@ifca.unican.es>
2014-07-09 17:35:11 +0200

    prepare config to split logs

--- config.py
+++ config.py
@@ -25,3 +25,8 @@ DB_PASSFILE = 'htpasswd.db'
 USELOGFILE = False
 LOGFILE = "trufa.log"
 #LOGFILE = "/var/genorama/log/trufa.log"
+LOGFILEBYTES = 500*1024
+
+USEWLOGFILE = True
+WLOGFILE = "trufa_web.log"
+#WLOGFILE = "/var/genorama/log/trufa_web.log"
