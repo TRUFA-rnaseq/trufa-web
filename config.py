5b5548fe2553abdd0ad02fe67fc187869be802f7
Luis Cabellos <cabellos@ifca.unican.es>
2015-02-19 13:28:55 +0100

    add more info at config file

--- config.py
+++ config.py
@@ -1,5 +1,31 @@
+######################################################################
 PROJECT_NAME = "trufa"
 VERSION = "0.12.0"
+
+######################################################################
+### Web Configurations ###
+
+SERVER_CRT = "cert/server.crt"
+SERVER_KEY = "cert/server.key"
+WEB_DEBUG = False
+
+EMAIL_JOB_COMPLETE = "mail/job_complete.txt"
+EMAIL_JOB_COMPLETE_SUBJECT = 'TRUFA: Your analysis "{}" all done !'
+EMAIL_SENDER = "kornobis@trufa.ifca.es"
+EMAIL_SMTP = "localhost"
+
+######################################################################
+### Database configuracion ###
+DB_RESET = True
+DB_DATABASE = 'database.db'
+DB_PASSFILE = 'htpasswd.db'
+
+DB_TEST_USER = "admin"
+DB_TEST_PASS = "admin"
+DB_TEST_EMAIL = "j.smith@example.com"
+
+######################################################################
+### Job/runner configuration ###
 
 REMOTEHOST = "genorama@altamira1.ifca.es"
 
@@ -20,14 +46,12 @@ LAUNCHER_PATH = '../server_side/lib'
 #PIPE_LAUNCH = "../../server_side/stable/pipe_launcher.py"
 #LAUNCHER_PATH = '/var/genorama/server_side/stable/lib'
 
-## Database Configurations
-DB_RESET = True
-DB_DATABASE = 'database.db'
-DB_PASSFILE = 'htpasswd.db'
-
 # Demo datasets
 DEMO_DIR = "/gpfs/res_projects/cvcv/webserver/demo_data/"
 DEMO_INFILES = [ "Spombe_left.fq", "Spombe_right.fq"]
+
+######################################################################
+### Log/debug configuration ###
 
 USELOGFILE = False
 LOGFILE = "trufa.log"
@@ -38,5 +62,8 @@ USEWLOGFILE = True
 WLOGFILE = "trufa_web.log"
 #WLOGFILE = "/var/genorama/log/trufa_web.log"
 
+######################################################################
 import logging
 logging.getLogger().setLevel( logging.DEBUG )
+
+######################################################################
