75841f7fa0394f764df1d731056d009b508b7526
Luis Cabellos <cabellos@ifca.unican.es>
2015-06-10 11:26:40 +0200

    apply pep8 style to pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 import logging
 import sys
 import multiprocessing
@@ -15,8 +15,8 @@ import httplib
 import json
 from cStringIO import StringIO
 
-#-------------------------------------------------------------------------------
-sys.path.append( config.USERS_LIB )
+# ------------------------------------------------------------------------------
+sys.path.append(config.USERS_LIB)
 try:
     import users
 except ImportError:
@@ -24,70 +24,74 @@ except ImportError:
     print "Check USERS_LIB at config file"
     exit(-1)
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 remotehost = config.REMOTEHOST
 remotehome = config.REMOTEHOME
 data_dir = config.DATADIR
 
 authority = config.LAUNCHER_SERVICE
 
-#-------------------------------------------------------------------------------
-def encodeRESTParams( params ):
-    payload = json.dumps( params, ensure_ascii=False )
-    payload.encode( 'utf-8' )
+
+# ------------------------------------------------------------------------------
+def encodeRESTParams(params):
+    payload = json.dumps(params, ensure_ascii=False)
+    payload.encode('utf-8')
 
     # define the params encoding
-    headers = { 'Content-Type': 'application/json; charset=utf-8'}
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
 
     return (payload, headers)
 
-#-------------------------------------------------------------------------------
-def getRESTResult( conn ):
+
+# ------------------------------------------------------------------------------
+def getRESTResult(conn):
     # get output from connection
     retValues = {}
     response = conn.getresponse()
     if response.status == 200:
         try:
-            retValues = json.loads( response.read() )
+            retValues = json.loads(response.read())
         except ValueError:
-            logging.error( "can't decode json response" )
+            logging.error("can't decode json response")
             return None
     else:
-        logging.error( "http error %d, %s"
-                       % (response.status, response.reason) )
+        logging.error("http error %d, %s"
+                      % (response.status, response.reason))
         return None
 
     # check error
-    if not retValues.get('ok' , False ):
-        logging.error( "REST return: "
-                       + retValues.get('errormsg', "unknown error") )
+    if not retValues.get('ok', False):
+        logging.error("REST return: "
+                      + retValues.get('errormsg', "unknown error"))
         return None
 
     return retValues
 
-#-------------------------------------------------------------------------------
-def callGetJobStatus( joblist ):
-    conn = httplib.HTTPConnection( authority )
 
-    payload, headers = encodeRESTParams( {'joblist': joblist} )
+# ------------------------------------------------------------------------------
+def callGetJobStatus(joblist):
+    conn = httplib.HTTPConnection(authority)
+
+    payload, headers = encodeRESTParams({'joblist': joblist})
 
     # call the remote service
     try:
-        conn.request( 'GET', '/jobs', body=payload, headers=headers )
+        conn.request('GET', '/jobs', body=payload, headers=headers)
     except:
-        logging.error( str(sys.exc_info()) )
+        logging.error(str(sys.exc_info()))
         return None
 
-    result = getRESTResult( conn )
+    result = getRESTResult(conn)
 
     if result is not None:
-        return result.get( 'jobs', [] )
+        return result.get('jobs', [])
 
     return []
 
-#-------------------------------------------------------------------------------
-def callRunJob( user, params ):
-    conn = httplib.HTTPConnection( authority )
+
+# ------------------------------------------------------------------------------
+def callRunJob(user, params):
+    conn = httplib.HTTPConnection(authority)
 
     # encode the request params
     params = {
@@ -96,120 +100,126 @@ def callRunJob( user, params ):
         'params': params,
         }
 
-    payload, headers = encodeRESTParams( params )
+    payload, headers = encodeRESTParams(params)
 
     # call the remote service
     try:
-        conn.request( 'PUT', '/jobs', body=payload, headers=headers )
+        conn.request('PUT', '/jobs', body=payload, headers=headers)
     except:
-        logging.error( str(sys.exc_info()) )
+        logging.error(str(sys.exc_info()))
         return None
 
-    result = getRESTResult( conn )
+    result = getRESTResult(conn)
 
     if result:
-        return result.get( 'jobid', None )
+        return result.get('jobid', None)
 
     return None
 
-#-------------------------------------------------------------------------------
-def callJobStatus( jobid ):
-    conn = httplib.HTTPConnection( authority )
+
+# ------------------------------------------------------------------------------
+def callJobStatus(jobid):
+    conn = httplib.HTTPConnection(authority)
 
     # call the remote service
     try:
-        conn.request( 'GET', '/jobs/'+str(jobid) )
+        conn.request('GET', '/jobs/'+str(jobid))
     except:
-        logging.error( str(sys.exc_info()) )
+        logging.error(str(sys.exc_info()))
         return None
 
-    result = getRESTResult( conn )
+    result = getRESTResult(conn)
 
     print result
 
     return result
 
-#-------------------------------------------------------------------------------
-def callCancelJob( jobid ):
-    conn = httplib.HTTPConnection( authority )
 
-    payload, headers = encodeRESTParams( {'cancel': True} )
+# ------------------------------------------------------------------------------
+def callCancelJob(jobid):
+    conn = httplib.HTTPConnection(authority)
+
+    payload, headers = encodeRESTParams({'cancel': True})
 
     # call the remote service
     try:
-        conn.request( 'POST', '/jobs/'+str(jobid),
-                      body=payload, headers=headers )
+        conn.request('POST', '/jobs/'+str(jobid),
+                     body=payload, headers=headers)
     except:
-        logging.error( str(sys.exc_info()) )
+        logging.error(str(sys.exc_info()))
         return None
 
-    result = getRESTResult( conn )
+    result = getRESTResult(conn)
 
     if result:
-        return result.get( 'ok', False )
+        return result.get('ok', False)
 
     return False
 
-#-------------------------------------------------------------------------------
-def startJob( user, var1 ):
-    logging.info( "RUNNIN JOB of '%s'", user )
-    jobid = callRunJob( user, var1 )
+
+# ------------------------------------------------------------------------------
+def startJob(user, var1):
+    logging.info("RUNNIN JOB of '%s'", user)
+    jobid = callRunJob(user, var1)
     if jobid is not None:
-        if db.insertNewJob( user, jobid ):
+        if db.insertNewJob(user, jobid):
             return True
 
-    raise RuntimeError( "Can't start new job for user " + user )
+    raise RuntimeError("Can't start new job for user " + user)
 
-#-------------------------------------------------------------------------------
-def cancelJob( user, jobid ):
-    logging.info( "CANCELING JOB %d", jobid )
-    jobinfo = db.getJobInfo( jobid )
-    logging.debug( str(jobinfo) )
+
+# ------------------------------------------------------------------------------
+def cancelJob(user, jobid):
+    logging.info("CANCELING JOB %d", jobid)
+    jobinfo = db.getJobInfo(jobid)
+    logging.debug(str(jobinfo))
 
     state = jobinfo['state']
     if state == db.JOB_COMPLETED or state == db.JOB_CANCELED:
-        logging.warning( "job %d already canceled", jobid )
+        logging.warning("job %d already canceled", jobid)
         return True
 
-    ret = callCancelJob( jobid )
+    ret = callCancelJob(jobid)
     if ret:
-        db.setJobCanceled( jobid )
+        db.setJobCanceled(jobid)
         return True
 
     # Removing job folders:
-        # Web and server job names should be corresponding before activating this
+    # Web and server job names should be corresponding before activating this
     # jname = jobinfo[ 'name' ]
     # print "Removing outputs from Job named: ", jname
     # command =  [ 'ssh', remotehost,
     #              'cd', data_dir + user,
     #              'rm -r', jname,
     #              'touch', "DONE"]
-    # proc = subprocess.Popen( command, stdout=subprocess.PIPE )
-    #output = proc.communicate()[0]
+    # proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    # output = proc.communicate()[0]
 
     return False
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def run():
-    p = multiprocessing.Process( target=pipelineLoop )
+    p = multiprocessing.Process(target=pipelineLoop)
     p.start()
     return p
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def sendJobCompletedEmail(jobid, username):
 
-    usermail = users.getUserEmail( username )
-    if usermail == None:
-        logging.warning( "User %d hasn't email", username )
+    usermail = users.getUserEmail(username)
+    if usermail is None:
+        logging.warning("User %d hasn't email", username)
         return
 
     jobinfo = db.getJobInfo(jobid)
 
     body = ""
-    with open( config.EMAIL_JOB_COMPLETE, 'r' ) as f:
+    with open(config.EMAIL_JOB_COMPLETE, 'r') as f:
         body = f.read()
 
-    msg = MIMEText( body.format(jobinfo['name'], jobinfo['juid']))
+    msg = MIMEText(body.format(jobinfo['name'], jobinfo['juid']))
 
     msg['Subject'] = config.EMAIL_JOB_COMPLETE_SUBJECT.format(jobinfo['name'])
     msg['From'] = config.EMAIL_SENDER
@@ -217,19 +227,20 @@ def sendJobCompletedEmail(jobid, username):
 
     print msg
 
-    s = smtplib.SMTP( config.EMAIL_SMTP )
-    s.sendmail( config.EMAIL_SENDER, usermail, msg.as_string())
+    s = smtplib.SMTP(config.EMAIL_SMTP)
+    s.sendmail(config.EMAIL_SENDER, usermail, msg.as_string())
     s.quit()
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def updatePipelineState():
     jobs = db.getActiveJobs()
     if len(jobs) > 0:
-        logging.info( "Checking %d job/s", len(jobs) )
+        logging.info("Checking %d job/s", len(jobs))
 
-    jobids = map( lambda j: j['jid'], jobs )
+    jobids = map(lambda j: j['jid'], jobs)
     if len(jobids) > 0:
-        jobstats = callGetJobStatus( jobids )
+        jobstats = callGetJobStatus(jobids)
 
         for stat in jobstats:
             jobid = stat['jobid']
@@ -240,41 +251,42 @@ def updatePipelineState():
                 'completed': db.JOB_COMPLETED,
                 'canceled': db.JOB_CANCELED,
                 'failed': db.JOB_FAILED,
-                }.get( stat['state'], db.JOB_CREATED )
+                }.get(stat['state'], db.JOB_CREATED)
 
-            jobinfo = db.getJobInfo( jobid )
+            jobinfo = db.getJobInfo(jobid)
             oldstate = jobinfo['state']
             if newstate == db.JOB_RUNNING and oldstate != db.JOB_RUNNING:
-                logging.info( "Job %d start RUNNING", jobid )
-                db.setJobRunning( jobid )
+                logging.info("Job %d start RUNNING", jobid)
+                db.setJobRunning(jobid)
 
             if newstate == db.JOB_COMPLETED:
                 # TODO stageout
 
-                logging.info( "Job %d COMPLETED", jobid )
-                db.setJobCompleted( jobid )
+                logging.info("Job %d COMPLETED", jobid)
+                db.setJobCompleted(jobid)
 
-                #username = db.getUserName( jobinfo['uid'] )
-                #sendJobCompletedEmail( jobinfo['jid'], username )
+                # username = db.getUserName(jobinfo['uid'])
+                # sendJobCompletedEmail(jobinfo['jid'], username)
 
             if newstate == db.JOB_CANCELED:
-                logging.info( "Job %d CANCELED", jobid )
-                db.setJobCompleted( jobid )
+                logging.info("Job %d CANCELED", jobid)
+                db.setJobCompleted(jobid)
 
             if newstate == db.JOB_FAILED:
-                logging.info( "Job %d FAILED", jobid )
-                db.setJobFailed( jobid )
+                logging.info("Job %d FAILED", jobid)
+                db.setJobFailed(jobid)
 
-#-------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def pipelineLoop():
 
     running = True
-    logging.info( "Start pipeline loop" )
-    while( running ):
+    logging.info("Start pipeline loop")
+    while(running):
         try:
 
-            time.sleep( 300 )
-            logging.info( "Update pipeline" )
+            time.sleep(300)
+            logging.info("Update pipeline")
             updatePipelineState()
 
         except KeyboardInterrupt:
@@ -282,9 +294,9 @@ def pipelineLoop():
             break
 
         except:
-            logging.error( "unknown error on loop: " + str(sys.exc_info()) )
+            logging.error("unknown error on loop: " + str(sys.exc_info()))
 
-    logging.info( "Ending pipeline loop" )
+    logging.info("Ending pipeline loop")
 
 
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
